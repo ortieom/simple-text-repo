bbffb3e30923908fe0f11d2d9069547301182942
Artyom Shaposhnikov <avshapos@gmail.com>
2023-06-19 12:05:59 +0300

    2.3 added Requests in DataLib

---
+++ DataLib/UserInfo.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLib {
+    public class UserInfo {
+        public ulong Id { get; set; }
+
+        public string Name { get; set; } = null!;
+        public string? Surname { get; set; }
+        public string Email { get; set; } = null!;
+        public string HashedPassword { get; set; } = null!;
+
+        public string ContactType { get; set; } = null!;
+        public string ContactValue { get; set; } = null!;
+
+        public ICollection<Project> Projects { get; set; } = null!;  // transparent many-to-many
+
+
+    }
+}
